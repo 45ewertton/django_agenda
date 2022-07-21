991ddddc0d46fd9420b379bafb3ee3d62f0fd9b8
ewertton oliveira <ewertton45mb@gmail.com>
2022-07-21 16:48:23 -0300

    Configuracoes no models, admin e utilizando condicionais no template

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,10 +3,11 @@ from .models import Categoria, Contato
 # Register your models here.
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     list_per_page = 5
     search_fields = ('id', 'nome', 'sobrenome', 'telefone')
+    list_editable = ('telefone', 'mostrar')
     
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
