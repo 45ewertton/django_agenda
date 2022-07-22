5bcc7fa8df3eed09d32cbf8f4abf1d3edae0f75c
ewertton oliveira <ewertton45mb@gmail.com>
2022-07-22 15:51:38 -0300

    Ordenando e filtrando valoes

--- contatos/views.py
+++ contatos/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
@@ -5,7 +6,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(mostrar=True)
     paginator = Paginator(contatos, 3)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -15,6 +16,10 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
         })
