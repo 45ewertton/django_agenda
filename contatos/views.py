4bc8e0c15cd576eac0561492a3074c1a08a68bf7
ewertton oliveira <ewertton45mb@gmail.com>
2022-07-25 17:17:47 -0300

    Campo de pesquisa

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 # Create your views here.
 
@@ -24,3 +26,15 @@ def ver_contato(request, contato_id):
         'contato': contato
         })
 
+def busca(request):
+    termo = request.GET.get('termo')
+    if termo is None:
+        raise Http404()
+    campos = Concat('nome', Value(''), 'sobrenome')
+    contatos = Contato.objects.annotate(nome_complemento=campos).filter(Q(nome_complemento__icontains=termo) | Q(telefone__icontains=termo))
+    paginator = Paginator(contatos, 3)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
+        })
